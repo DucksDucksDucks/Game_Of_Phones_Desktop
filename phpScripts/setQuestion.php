1d80f2f685247f109b29f5c81a9db981060cd738
Zgreen0325 <zgreen@drury.edu>
2017-04-06 16:42:46 -0500

    UPDATE function
    Update function now works, but no insert check

--- phpScripts/setQuestion.php
+++ phpScripts/setQuestion.php
@@ -1,30 +1,20 @@
 <?php
 
-    $questionID = $_POST['qID'];
-    $deviceID = $_POST['deviceID'];;
-	
-	include_once '/database/dataManager.php';
+    function setQuestion($manager, $qID, $deviceID) {
 
-    $manager = new DataManager;
-    $manager->connect();
+        $manager = new DataManager;
+        $manager->connect();
 
-    // set up a prepared statement to modify database
+        // set up a prepared statement to modify database
 
-    $check = "SELECT * from currentquestion WHERE m_device_id = ".$deviceID.";";
-    $checkValues = array($deviceID);
+        $query = "UPDATE currentquestion SET q_id=".$qID." WHERE m_device_id = ".$deviceID.";";
 
-    
-    $checkResult = $manager->doQuery($check);
+        // connect to the database and execute the query.	
+        $result = $manager->doNonQuery($query,true,null);
 
-    if (mysql_num_rows($checkResult)==0) {
-        $query = "INSERT INTO currentquestion VALUES (".deviceID.", ".questionID.");";
-    /*} else {
-        $query = "UPDATE currentquestion SET q_id=".$questionID." WHERE m_device_id = ".$deviceID.";";
+        $manager->disconnect();
+
+        return "success";
     }
-    */
-    // connect to the database and execute the query.	
-    $result = $manager->doNonQuery($query,true,null);
-
-    $manager->disconnect();
 
 ?>
